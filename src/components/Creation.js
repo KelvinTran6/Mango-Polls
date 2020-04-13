df9808269e3dbc4c858f9b66bc1bb51334aa34b9
Kelvin <kelvintran4711@gmail.com>
2020-04-12 18:45:58 -0600

    implemented adding poll functionality, made the creation page look better

--- src/components/Creation.js
+++ src/components/Creation.js
@@ -19,7 +19,16 @@ class Creation extends React.Component {
   }
 
   submit = () => {
-    console.log(this.state.abber)
+
+    if(this.state.title == ''){
+      alert("title cannot be empty!")
+      return;
+    }
+
+    if(this.state.abber.length <2){
+      alert("there must be atleast two answers")
+      return
+    }
 
     const data = {
       title: this.state.title,
@@ -72,49 +81,53 @@ class Creation extends React.Component {
   render() {
     return (
       <div className="content">
-        <Grid container>
-          <Grid xs={0} sm={3} />
-          <Card>
-            <Grid container item
-              container
-              alignItems="center"
-              justify="center">
-              <Grid item xs={12} sm={8}>
-                <CardContent>
-                  <TextField fullWidth value={this.state.value} onChange={this.changeTitle} id="filled-search" label="Enter a Question" type="search" variant="standard" />
-                  {
-                    this.state.abber.map((text, index) => {
-                      return (
-                        <div key={index} className="answers">
-                          <TextField value={text} onChange={(e) => this.onChangeAnswers(e, index)} id="filled-search" label="answer" type="search" variant="standard" />
-                        </div>
-                      )
-                    })
-                  }
-                </CardContent>
+        <Grid container direction = "column">
+          <Grid item container>
+            <Grid container item xs={0} sm={3} />
+              <Grid xs = {12} sm = {6}>
+                <Card>
+                  <Grid container item
+                    container
+                    alignItems="center"
+                    justify="center">
+                    <Grid item xs={12} sm={8}>
+                      <CardContent>
+                        <TextField fullWidth value={this.state.value} onChange={this.changeTitle} id="filled-search" label="Enter a Question" type="search" variant="standard" />
+                        {
+                          this.state.abber.map((text, index) => {
+                            return (
+                              <div key={index} className="answers">
+                                <TextField value={text} onChange={(e) => this.onChangeAnswers(e, index)} id="filled-search" label="answer" type="search" variant="standard" />
+                              </div>
+                            )
+                          })
+                        }
+                      </CardContent>
+                    </Grid>
+                    <Grid container item xs={12} sm={8}>
+                      <div className="btn">
+                        <Grid item>
+                          <CardActions>
+                            <Button onClick={() => this.add()} size="small"> add answer </Button>
+                          </CardActions>
+                        </Grid>
+                        <Grid item>
+                          <CardActions>
+                            <Button onClick={() => this.remove(0)} size="small"> remove answer </Button>
+                          </CardActions>
+                        </Grid>
+                        <Grid item>
+                          <CardActions>
+                            <Button onClick={() => this.submit()} size="small"> Create Poll </Button>
+                          </CardActions>
+                        </Grid>
+                      </div>
+                    </Grid>
+                  </Grid>
+                </Card>
               </Grid>
-              <Grid container item xs={12} sm={8}>
-                <div className="btn">
-                  <Grid item>
-                    <CardActions>
-                      <Button onClick={() => this.add()} size="small"> add answer </Button>
-                    </CardActions>
-                  </Grid>
-                  <Grid item>
-                    <CardActions>
-                      <Button onClick={() => this.remove(0)} size="small"> remove answer </Button>
-                    </CardActions>
-                  </Grid>
-                  <Grid item>
-                    <CardActions>
-                      <Button onClick={() => this.submit()} size="small"> Create Poll </Button>
-                    </CardActions>
-                  </Grid>
-                </div>
-              </Grid>
-            </Grid>
-          </Card>
-          <Grid xs={0} sm={3} />
+            <Grid item xs={0} sm={3} />
+          </Grid>
         </Grid>
 
       </div>
